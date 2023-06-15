d6bf21703d995c53cdb0a89b70417b61d899e794
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-06-15 13:07:10 +0530

    complete setup product listing - amazon scrapper api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const { config } = require("dotenv");
 const { protect } = require("./authentication/protect");
+const apicache = require("apicache");
 
 const app = express();
 
@@ -19,9 +20,10 @@ require("./models");
 // ---------------------Routes--------------
 const productRoutes = require("./routes/product.routes");
 const userRoutes = require("./routes/user.routes");
-
+const productRouter = require("./api/productApi");
 // Model
 const db = require("./models");
+
 const userModel = (req, res, next) => {
   req.model = db.user;
   next();
@@ -33,16 +35,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
 app.use(morgan("dev"));
 
+//configure apicache
+let cache = apicache.middleware;
+
 // server static files
+
+//caching all routes for 5 minutes
+app.use(cache("5 minutes"));
 
 // Routing
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+// product
 app.use("/api", productRoutes);
 
+// user
 app.use("/api", userRoutes);
+
+// for product listing
+app.use("/api/product", productRouter);
 
 app.listen(PORT, () => {
   console.log("app is running " + PORT);
