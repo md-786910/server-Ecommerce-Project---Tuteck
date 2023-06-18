3b77e94bc8603f9ff46e32460eb49925e2b1f641
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-06-18 17:36:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,20 @@ const userModel = (req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
+
+app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+      );
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+      }
+      next();
+    });
+
 app.use(morgan("dev"));
 
 //configure apicache
